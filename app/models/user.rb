6c81284a3e318e60048e0c701e6b52f5f21a95df
Kane Baccigalupi <baccigalupi@gmail.com>
2009-01-26 17:40:30 -0800

    initial build and fix for AR

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  # username
+  # salt
+  # crypted_password
+  # timestamps
+  
+  # auth_token # used for verification, then for remember token
+  # remember_until
+  
+  # status
+  # roles
+
+  # first_name
+  # last_name
+  # name_visibility
+  # address
+  # zipcode
+
+  
+end
---
+++ config/dependencies.rb
@@ -0,0 +1,28 @@
+merb_gems_version = "1.0.8.1"
+ar_gems_version   = "2.2.2"
+
+dependency "merb-action-args",          merb_gems_version
+dependency "merb-assets",               merb_gems_version  
+dependency "merb-cache",                merb_gems_version   
+dependency "merb-helpers",              merb_gems_version 
+dependency "merb-mailer",               merb_gems_version  
+dependency "merb-slices",               merb_gems_version  
+dependency "merb-auth",                 merb_gems_version
+dependency "merb-auth-core",            merb_gems_version
+dependency "merb-auth-more",            merb_gems_version
+dependency "merb-auth-slice-password",  merb_gems_version
+dependency "merb-param-protection",     merb_gems_version
+dependency "merb-exceptions",           merb_gems_version
+dependency "merb-gen",                  merb_gems_version
+
+dependency "activesupport",             ar_gems_version
+dependency "activerecord",              ar_gems_version
+dependency "merb_activerecord"
+
+dependency "RedCloth"
+#dependency "rughetto-merb_paperclip",   :require_as => "merb_paperclip"
+dependency "rughetto-rear_views",       :require_as => "rear_views"
+
+# testing factories!
+dependency "notahat-machinist",         :require_as => "machinist"
+dependency "faker"
